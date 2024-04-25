01bb91c063b188ab3e82b7991ffd94929fcb418c
UWE-i <iuwe@MacBook-Air-von-Cayan.local>
2024-04-25 15:23:33 +0200

    connecting db again

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,6 @@ const app = express();
 const port = 3001;
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
