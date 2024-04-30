8ee0bbb89c9c166443cab4ca297fbfa0a0154ced
codeUWE <code.cayan@gmail.com>
2024-04-30 17:16:46 +0200

    structure frontend with routes and fonts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 //components
 import Tasks from './components/Tasks';
-
-import Actionbar from './components/Actionbar';
+import SignUp from './components/SignUp';
+import LoginCard from './components/LoginCard';
+import NavBar from './components/NavBar';
+import Dashboard from './components/Dashboard';
+import Layout from './components/Layout';
+import Team from './components/Team';
+import Notes from './components/Notes';
 
 function App() {
 	return (
 		<>
-			<Tasks />
-			<Actionbar />
+			<main>
+				<Routes>
+					<Route path="/login" element={<LoginCard />} />
+					<Route path="/signup" element={<SignUp />} />
+					<Route path="/" element={<Layout />}>
+						<Route path="/dashboard" element={<Dashboard />} />
+						<Route path="/tasks" element={<Tasks />} />
+						<Route path="/team" element={<Team />} />
+						<Route path="/notes" element={<Notes />} />
+					</Route>
+				</Routes>
+			</main>
 		</>
 	);
 }
