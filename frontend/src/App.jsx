30a873129cbcc351a7bfa149c5bab936a8659692
codeUWE <code.cayan@gmail.com>
2024-04-30 10:47:10 +0200

    installing the frontend with tailwind and material tailwind libary component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from '@material-tailwind/react';
+
+function App() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold text-red-600 underline">
+				Hello world!
+			</h1>
+			<Button>Button</Button>
+		</>
+	);
+}
+
+export default App;
