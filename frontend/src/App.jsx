612b3137f6fd32db61e7222fbac2025b72458e05
Reshma <reshmakulkarni02@gmail.com>
2024-05-07 10:24:49 +0200

    Fix App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,30 @@
-<<<<<<< HEAD
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 //components
-import Tasks from "./components/Tasks";
+import Task from "./components/Task";
+import SingleTaskBoard from "./components/SingleTaskBoard";
+// import SignUp from './components/SignUp';
 import SignUpCard from "./components/SignUpCard";
 import LoginCard from "./components/LoginCard";
+import AdminDashboard from "./components/AdminDashboard";
 import Dashboard from "./components/Dashboard";
 import Layout from "./components/Layout";
 import Team from "./components/Team";
 import Notes from "./components/Notes";
-import AdminDashboard from "./components/AdminDashboard";
+import GoodNews from "./components/GoodNews";
+import WidgetOpenSubTasks from "./components/WidgetOpenSubTasks";
+import Schedule from "./components/Schedule";
+// import SampleWidget from './components/SampleWidget';
 
 function App() {
   return (
     <>
+      <Schedule />
+      <GoodNews />
+      {/* <SampleWidget/> */}
+      <WidgetOpenSubTasks />
+
       <main>
         <Routes>
           <Route path="/login" element={<LoginCard />} />
@@ -22,7 +32,8 @@ function App() {
           <Route path="/admindashboard" element={<AdminDashboard />} />
           <Route path="/" element={<Layout />}>
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/tasks" element={<Task />} />
+            <Route path="/tasks/:id" element={<SingleTaskBoard />} />
             <Route path="/team" element={<Team />} />
             <Route path="/notes" element={<Notes />} />
           </Route>
@@ -30,53 +41,6 @@ function App() {
       </main>
     </>
   );
-=======
-import './App.css';
-import {Routes, Route} from "react-router-dom";
-
-//components
-import Task from './components/Task';
-import SingleTaskBoard from './components/SingleTaskBoard';
-// import SignUp from './components/SignUp';
-import SignUpCard from './components/SignUpCard';
-import LoginCard from './components/LoginCard';
-import Dashboard from './components/Dashboard';
-import Layout from './components/Layout';
-import Team from './components/Team';
-import Notes from './components/Notes';
-import GoodNews from './components/GoodNews';
-import WidgetOpenSubTasks from './components/WidgetOpenSubTasks';
-import Schedule from './components/Schedule';
-// import SampleWidget from './components/SampleWidget';
-
-
-
-function App() {
-	return (
-		<>
-		<Schedule/>
-<GoodNews/>
-{/* <SampleWidget/> */}
-<WidgetOpenSubTasks/>
-
-			<main>
-				<Routes>
-					<Route path="/login" element={<LoginCard />} />
-					<Route path="/signup" element={<SignUpCard />} />
-					<Route path="/" element={<Layout />}>
-						<Route path="/dashboard" element={<Dashboard />} />
-						<Route path="/tasks" element={<Task />} />
-						<Route path="/tasks/:id" element={<SingleTaskBoard />} />
-						<Route path="/team" element={<Team />} />
-						<Route path="/notes" element={<Notes />} />
-						
-					</Route>
-				</Routes>
-			</main>
-
-		</>
-	);
->>>>>>> main
 }
 
 export default App;
