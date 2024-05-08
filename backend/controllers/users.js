d1a13fd2f7810ed142c0b2d1e7e3d53a18e49581
Reshma <reshmakulkarni02@gmail.com>
2024-05-08 23:10:06 +0200

    Add useContext and adminDashboard

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,83 +1,95 @@
-const User = require('../models/users');
+const User = require("../models/users");
+const bcrypt = require("bcrypt");
 
 const getUsers = async (req, res) => {
-	try {
-		// const { isActive } = req.query;
-		const query = {};
+  try {
+    // const { isActive } = req.query;
+    const query = {};
 
-		// if (isActive === 'true') query.isActive = true;
-		// if (isActive === 'false') query.isActive = false;
+    // if (isActive === 'true') query.isActive = true;
+    // if (isActive === 'false') query.isActive = false;
 
-		const users = await User.find(query);
-		// //without using query - showing only the ones which are active
-		// // const users = await User.find({ isActive: true });
-		res.json(users);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json('Something went wrong');
-	}
+    const users = await User.find(query);
+    // //without using query - showing only the ones which are active
+    // // const users = await User.find({ isActive: true });
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Something went wrong");
+  }
 };
 const getUser = async (req, res) => {
-	try {
-		const { id } = req.params;
-		const user = await User.findById(id).populate({ 
-			path: 'roles',
-			populate: {
-			  path: 'permissions',
-			  model: 'Permission'
-			} 
-		 });
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).populate({
+      path: "roles",
+      populate: {
+        path: "permissions",
+        model: "Permission",
+      },
+    });
 
-		res.json(user);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json('Something went wrong');
-	}
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Something went wrong");
+  }
 };
 
 const createUser = async (req, res) => {
-	try {
-		const {  name, age, email,password,profilePicture,socialConnections,availability,roles } = req.body;
-		const createdUser = await User.create({
-			name,
-			age,
-            email,
-			password,
-            profilePicture,
-            socialConnections,
-            availability,
-            roles,
-          
-	
-		});
-		res.json(createdUser);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json('Something went wrong');
-	}
+  // console.log(`Inside create User ${req.body}`);
+  try {
+    const {
+      name,
+      age,
+      email,
+      password,
+      profilePicture,
+      socialConnections,
+      availability,
+      role,
+    } = req.body;
+    const found = await User.findOne({ email });
+    if (found) throw new Error("User already Exists");
+    const hash = await bcrypt.hash(password, 10);
+    const createdUser = await User.create({
+      name,
+      age,
+      email,
+      password: hash,
+      profilePicture,
+      socialConnections,
+      availability,
+      role,
+    });
+    res.json(createdUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Something went wrong");
+  }
 };
 const updateUser = async (req, res) => {
-	try {
-		const {
-			body,
-			params: { id },
-		} = req;
-		const updatedUser = await User.findByIdAndUpdate(id, body, { new: true });
-		res.send(updatedUser);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json('Something went wrong');
-	}
+  try {
+    const {
+      body,
+      params: { id },
+    } = req;
+    const updatedUser = await User.findByIdAndUpdate(id, body, { new: true });
+    res.send(updatedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Something went wrong");
+  }
 };
 const deleteUser = async (req, res) => {
-	try {
-		const { id } = req.params;
-		const deletedUser = await User.findByIdAndDelete(id);
-		res.send(deletedUser);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json('Something went wrong');
-	}
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+    res.send(deletedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Something went wrong");
+  }
 };
 
 module.exports = { getUsers, getUser, createUser, updateUser, deleteUser };
