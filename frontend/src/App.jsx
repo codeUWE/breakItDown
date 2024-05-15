d1a594a57a4ef0fade259f3617a00a5a6443e9e1
Reshma <reshmakulkarni02@gmail.com>
2024-05-15 09:43:14 +0200

    Manage Users added and navbar updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,29 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 //components
-import Task from './components/Task';
-import SingleTaskBoard from './components/SingleTaskBoard';
+import Task from "./components/Task";
+import SingleTaskBoard from "./components/SingleTaskBoard";
 
 // import SignUp from './components/SignUp';
 import SignUpCard from "./components/SignUpCard";
 import LoginCard from "./components/LoginCard";
-import AdminDashboard from "./components/AdminDashboard";
+import AdminDashboard from "./components/CreateUserForm";
 import ManageProjects from "./components/ManageProjects";
 import ManageRoles from "./components/ManageRoles";
 import ManageUsers from "./components/ManageUsers";
 import Dashboard from "./components/Dashboard";
 import Layout from "./components/Layout";
-import Team from "./components/Team";
+// import Team from "./components/Team";
 import Notes from "./components/Notes";
 
 import Schedule from "./components/Schedule";
 import Authorize from "./components/Authorize";
 
-import ProjectProgress from './components/ProjectProgress';
-import TeamView from './components/TeamView';
+import ProjectProgress from "./components/ProjectProgress";
+import TeamView from "./components/TeamView";
 
 function App() {
-
   return (
     <>
       <main>
@@ -41,18 +40,16 @@ function App() {
             </Route>
 
             <Route path="/dashboard" element={<Dashboard />} />
-						<Route path="/tasks" element={<Task />} />
-						<Route path="/Team" element={<TeamView/>} />
-						<Route path="/tasks/:id" element={<SingleTaskBoard />} />
-						<Route path="/tasks/progress" element={<ProjectProgress />} />
-						<Route path="/notes" element={<Notes />} />
+            <Route path="/tasks" element={<Task />} />
+            <Route path="/Team" element={<TeamView />} />
+            <Route path="/tasks/:id" element={<SingleTaskBoard />} />
+            <Route path="/tasks/progress" element={<ProjectProgress />} />
+            <Route path="/notes" element={<Notes />} />
           </Route>
         </Routes>
       </main>
     </>
   );
-
-
 }
 
 export default App;
