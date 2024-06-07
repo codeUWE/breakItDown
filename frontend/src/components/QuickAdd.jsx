329b0265caf82d51b6d547b29ed8d68ba5dd3a52
UWE-i <iuwe@MacBook-Air-von-Cayan.local>
2024-06-07 14:41:22 +0200

    fixing

--- frontend/src/components/QuickAdd.jsx
+++
@@ -1,9 +0,0 @@
-const QuickAdd = ({ onClick }) => {
-	return (
-		<button className="rounded-lg border-2 border-black" onClick={onClick}>
-			Quick Add +
-		</button>
-	);
-};
-
-export default QuickAdd;
